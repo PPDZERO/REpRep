db0e0059228cc449ed1bcbb03a0e83a1683ceabe
PPDZERO <dota.buy@yandex.ru>
2020-03-03 21:32:27 +0300

    Delete any string

--- Ya.Maps/venv/features/steps/success_login_steps.py
+++ Ya.Maps/venv/features/steps/success_login_steps.py
@@ -11,13 +11,6 @@ def
 def tap_account(context):
     context.app.menu_page.tap_account()
 
-@and('Вводит валидный логин')
-def
-
-@and('И валидный пароль')
-def
-
-
 @then('Пользователь успешно авторизовался')
 def sign_in_displayed(context):
     context.app.account_page.sign_in_displayed()
